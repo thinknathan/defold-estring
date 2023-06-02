c4317ea099edf7c64fb03e487b43fe10c157033a
Nathan Bolton <nathan@nathanbolton.ca>
2023-06-01 17:18:28 -0700

    Update types

--- @types/defold-estring.d.ts
+++ @types/defold-estring.d.ts
@@ -2,60 +2,73 @@
 
 /**
  * Functions for interacting with the eString native extension
+ * @noResolution
  */
 declare namespace estring {
 	/**
 	 * Joins two strings
-	 * @param first 
-	 * @param second 
+	 * @param first
+	 * @param second
 	 */
-	export function concat(first: string, second:string): string
+	export function concat(first: string, second: string): string;
 	/**
 	 * Replace each occurance of `search` in `subject` with `replace` value
-	 * @param subject 
-	 * @param search 
-	 * @param replace 
+	 * @param subject
+	 * @param search
+	 * @param replace
 	 */
-	export function replace(subject: string, search: string, replace: string): string
+	export function replace(
+		subject: string,
+		search: string,
+		replace: string
+	): string;
 	/**
 	 * Trim whitespace from input
-	 * @param input 
+	 * @param input
 	 */
-	export function trim(input: string): string
+	export function trim(input: string): string;
 	/**
 	 * Create a table with values from `input`, split by `delimiter`
-	 * @param input 
-	 * @param delimiter 
+	 * @param input
+	 * @param delimiter
 	 */
-	export function split(input: string, delimiter: string): string[]
+	export function split(input: string, delimiter: string): string[];
 	/**
 	 * Joins values in a `table` into one string, with a `separator` added between
-	 * @param table 
-	 * @param separator 
+	 * @param table
+	 * @param separator
 	 */
-	export function join(table: object, separator: string): string
+	export function join(table: object, separator: string): string;
 	/**
 	 * Adds `padding` to start of `input` string until it reaches specified `length`
-	 * @param input 
-	 * @param padding 
-	 * @param length 
+	 * @param input
+	 * @param padding
+	 * @param length
 	 */
-	export function pad_start(input: string, padding: string, length: number): string
+	export function pad_start(
+		input: string,
+		padding: string,
+		length: number
+	): string;
 	/**
 	 * Adds `padding` to end of `input` string until it reaches specified `length`
-	 * @param input 
-	 * @param padding 
-	 * @param length 
+	 * @param input
+	 * @param padding
+	 * @param length
 	 */
-	export function pad_end(input: string, padding: string, length: number): string
+	export function pad_end(
+		input: string,
+		padding: string,
+		length: number
+	): string;
 	/**
 	 * Adds comma every 3 digits as a thousands separator
-	 * @param input 
+	 * @param input
 	 */
-	export function format_number(input: string | number): string
+	export function format_number(input: string | number): string;
 	/**
 	 * Separates number of seconds into hours, minutes, and seconds with a colon in-between. Skips hours digits if they are zero.
-	 * @param input 
+	 * @param input
 	 */
-	export function format_time(input: string | number): string
+	export function format_time(input: string | number): string;
 }
